c6db5367631ba4ea9eee59a73a2c518018df5b9a
mattpen <matthew.pennington@colorado.edu>
2019-03-12 23:09:59 -0600

    added functionality

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const express = require( 'express' );
 const https = require( 'https' );
 const PORT = process.env.PORT || 5000;
 
-console.log('wutup')
 express()
   .use( bodyParser.json() )
+  .use( bodyParser.urlencoded( { extended: true } ) )
   .post( '/dialog-flow', ( appReq, appRes ) => {
-    console.log(appReq);
+    console.log(appReq.body);
     https.get( 'https://api.citybik.es/v2/networks/boulder', bikeRes => {
       bikeRes.on( 'data', d => {
         d = JSON.parse( d );
         let station = d.network.stations.find( s => s.id === '974444d749237ed7552167e388dd2210' );
         if ( appReq.body.queryResult.intent.displayName === 'numbikes' ) {
-          res.json( {fulfillmentText: `There are ${ station.free_bikes } bikes available at ${ station.name }` } )
+          appRes.json( {fulfillmentText: `There are ${ station.free_bikes } bikes available at ${ station.name }` } )
         }
         else if ( appReq.body.queryResult.intent.displayName === 'numslots' ) {
-          res.json( {fulfillmentText: `There are ${ station.empty_slots } bikes available at ${ station.name }` } )
+          appRes.json( {fulfillmentText: `There are ${ station.empty_slots } empty slots available at ${ station.name }` } )
         }
       } );
     } ).on( 'error', e => {
